283bfe0cd8cbc2830a62ad45a64cfca99d510687
Douglas Jasper <doug.jasper@trios.com>
2025-02-26 08:13:55 -0500

    Initial Commit

---
+++ FlipCardDemo/FlipCardDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipCardDemoApp.swift
+//  FlipCardDemo
+//
+//  Created by Douglas Jasper on 2025-02-26.
+//
+
+import SwiftUI
+
+@main
+struct FlipCardDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
